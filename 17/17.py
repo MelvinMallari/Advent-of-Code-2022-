40102f9ee73c49b16e1bf666c6a9020b541d8928
Melvin Mallari <melvintmallari@gmail.com>
2022-12-21 16:11:57 -0800

    17 pt 2

--- 17/17.py
+++ 17/17.py
@@ -10,29 +10,50 @@ jets = [1 if l == '>' else -1 for l in open('17.txt').read()]
 solid = {x - 1j for x in range(7)}
 lwall, rwall = -1, 7
 height = 0
+seen = {}
 
 num_rocks = 0
-NUM_SIMS = 2022
+TARGET_ROCKS = 1000000000000
 rock = {r.real + 2 + (r.imag + height + 3)*1j for r in rocks[0]}
-a = []
 
-while num_rocks < NUM_SIMS:
-  if num_rocks % 100 == 0: print(num_rocks)
-  for jet in jets:
+# summarize the top skyline for the last 20 rows
+def summarize():
+  o = [-20]*7
+
+  for x in solid:
+    r = int(x.real)
+    i = int(x.imag)
+    o[r] = max(o[r], i)
+  
+  t = max(o)
+  return tuple(x - t for x in o)
+offset = 0
+
+while num_rocks < TARGET_ROCKS:
+  for j, jet in enumerate(jets):
     moved = {r + jet for r in rock}
     if all(lwall < m.real < rwall for m in moved) and not (moved & solid):
       rock = moved
     moved = {r - 1j for r in rock} 
     if (moved & solid):
       solid |= rock
-      o = height
-      height = max((s.imag for s in solid)) + 1
-      a.append(int(height - o))
+      height = int(max((s.imag for s in solid)) + 1)
       num_rocks += 1
-      if num_rocks >= NUM_SIMS: break
-      rock = {r.real + 2 + (r.imag + height + 3)*1j for r in rocks[num_rocks%5]}
+      rock_index = num_rocks%5
+      key = (rock_index, j, summarize())
+      if key in seen:
+        cycle_start_rocks, cycle_start_height = seen[key]
+        cycle_rocks = num_rocks - cycle_start_rocks
+        cycle_height = height - cycle_start_height
+        rocks_remaining = TARGET_ROCKS - num_rocks
+        num_cycles = rocks_remaining // cycle_rocks
+        offset = num_cycles * cycle_height
+        num_rocks += num_cycles * cycle_rocks
+        seen = {}
+      seen[key] = (num_rocks, height)
+      if num_rocks >= TARGET_ROCKS: break
+      rock = {r.real + 2 + (r.imag + height + 3)*1j for r in rocks[rock_index]}
     else:
       rock = moved
 
-print(a)
-print(int(height))
+print(height + offset)
